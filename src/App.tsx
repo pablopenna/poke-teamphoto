dbe593b3e1a1a6ff60880b4d40a3dbd5f1c87807
Pablo Peña <pablo_penna7@hotmail.com>
2025-11-27 14:58:17 +0800

    feat: fabric.js init setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as fabric from 'fabric'; // Fabric.js
+import { FabricText } from 'fabric';
 
 function App() {
+  const canvasEl = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const options = {};
+    const canvas = new fabric.Canvas(canvasEl.current || undefined, options);
+    
+    // Create a rectangle and add it to the canvas
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      width: 200,
+      height: 100,
+      fill: 'red',
+      stroke: 'blue',
+      strokeWidth: 2
+    });
+
+    // Create text and add it to the canvas
+    const text = new FabricText('Hello Fabric!', {
+      left: 150,
+      top: 250,
+      fontSize: 20,
+      fill: 'white',
+    });
+
+    canvas.add(rect, text);
+
+    // Make the fabric.Canvas instance available to your app      
+    return () => {
+      canvas.dispose(); // Cleanup
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ width: '100%', height: '500px' }}>
+          <canvas width="1000" height="400" ref={canvasEl} />
+        </div>
+        <span>Hello?</span>
       </header>
     </div>
   );

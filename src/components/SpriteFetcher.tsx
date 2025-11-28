d9dadd16462f77fa696a989d236c22f526536896
Pablo Peña <pablo_penna7@hotmail.com>
2025-11-28 12:54:31 +0800

    feat: add input to enter name or id of poke to add to canvas

--- src/components/SpriteFetcher.tsx
+++ src/components/SpriteFetcher.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { fetchPokeSprite } from '../api';
 import * as fabric from 'fabric';
 import Button from '@mui/material/Button';
+import { HBox } from './common/layout';
+import { TextField } from '@mui/material';
 
 interface SpriteFetcherProps {
   canvasRef: React.RefObject<fabric.Canvas | null>;
 }
 
 export const SpriteFetcher: React.FC<SpriteFetcherProps> = ({ canvasRef }) => {
+
+  const [pokeId, setPokeId] = useState<string>();
+
   const getPokeSprite = (id: string) => {
     fetchPokeSprite(id).then(onSpriteDataFetched);
   };
@@ -27,9 +32,27 @@ export const SpriteFetcher: React.FC<SpriteFetcherProps> = ({ canvasRef }) => {
     });
   };
 
+  const onAddButtonClicked = (_event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if(!pokeId) {
+      return;
+    }
+
+    getPokeSprite(pokeId);
+  };
+
   return (
-    <Button variant="contained" onClick={() => getPokeSprite('ditto')}>
-      Get Poke
-    </Button>
+    <HBox className="slightly-gapped">
+      <TextField 
+        label="Name or Dex no." 
+        value={pokeId} 
+        onChange={(event) => {
+          const value = event.target.value;
+          setPokeId(value);
+        }}
+      />
+      <Button variant="contained" onClick={onAddButtonClicked}>
+        Add
+      </Button>
+    </HBox>
   );
 };

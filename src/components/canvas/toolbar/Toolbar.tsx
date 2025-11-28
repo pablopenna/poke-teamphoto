5a40dd6a28400903d3a3b16ec30e489c0fc4c471
Pablo Peña <pablo_penna7@hotmail.com>
2025-11-28 20:38:56 +0800

    feat: add wrapping to toolbar

--- src/components/canvas/toolbar/Toolbar.tsx
+++ src/components/canvas/toolbar/Toolbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import * as fabric from 'fabric';
-import { VBox } from '../../common/layout';
+import { Flexbox } from '../../common/layout';
 import { ResetSize, BringForwards, FlipImage, ManageBackground, RemoveImage, Duplicate, BringBackwards, SaveAsPng } from '.';
+import { ButtonGroup } from '@mui/material';
 
 interface CanvasToolbarProps {
   canvasRef: React.RefObject<fabric.Canvas | null>;
@@ -11,15 +12,17 @@ export const CanvasToolbar: React.FC<CanvasToolbarProps> = ({
   canvasRef,
 }) => {
    return (
-    <VBox className="slightly-gapped">
-      <BringForwards canvasRef={canvasRef}/>
-      <BringBackwards canvasRef={canvasRef}/>
+    <Flexbox className="slightly-gapped flex-wrap vbox">
+      <ButtonGroup>
+        <BringForwards canvasRef={canvasRef}/>
+        <BringBackwards canvasRef={canvasRef}/>
+      </ButtonGroup>
       <ManageBackground canvasRef={canvasRef}/>
       <FlipImage canvasRef={canvasRef}/>
       <ResetSize canvasRef={canvasRef}/>
       <RemoveImage canvasRef={canvasRef}/>
       <Duplicate canvasRef={canvasRef}/>
       <SaveAsPng canvasRef={canvasRef}/>
-    </VBox>
+    </Flexbox>
   );
 };
